8dbcfeddf28b742361c15a086fa35bb48fe46f39
IT01 <acp.it01@anphatplastic.com>
2026-09-21 15:59:45 +0700

    sadsad

--- app/data/apps.ts
+++ app/data/apps.ts
@@ -30,7 +30,10 @@ export const APP_CATEGORIES = [
   '💼 Sự nghiệp & Tài chính',
   '📚 Học tập & Trí tuệ',
   '🎨 Sáng tạo & Nghệ thuật',
-  '🏃 Sức khoẻ & Lối sống'
+  '🏃 Sức khoẻ & Lối sống',
+  '🤳 Mạng xã hội & Viral',
+  '🎮 Game & Thử thách',
+  '🤖 AI & Công nghệ'
 ]
 
 const zodiacOptions = ['Bạch Dương', 'Kim Ngưu', 'Song Tử', 'Cự Giải', 'Sư Tử', 'Xử Nữ', 'Thiên Bình', 'Bọ Cạp', 'Nhân Mã', 'Ma Kết', 'Bảo Bình', 'Song Ngư']
